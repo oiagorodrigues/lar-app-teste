d6b1beb0bfae8fa8a5e8d398b9bb9aa1aea06e89
Iago <yagomello16@gmail.com>
2019-03-12 03:42:26 -0300

    baseInput component

---
+++ src/components/base/BaseInput.vue
@@ -0,0 +1,76 @@
+<template>
+  <b-form-group
+    class="base-form-group px-2"
+    :label="label"
+    label-class="pb-0 text-uppercase text-info fontsize-12"
+  >
+    <b-form-input
+      v-bind="$attrs"
+      class="base-form-input"
+      v-model="localValue"
+      :placeholder="placeholder"
+      :type="type"
+    />
+  </b-form-group>
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+
+  inheritAttrs: false,
+
+  props: {
+    value: {
+      type: [String, Number],
+      default: () => ''
+    },
+
+    label: {
+      type: String,
+      default: ''
+    },
+
+    placeholder: {
+      type: String,
+      default: ''
+    },
+
+    type: {
+      type: String,
+      default: 'text'
+    }
+  },
+
+  data () {
+    return {}
+  },
+
+  computed: {
+    localValue: {
+      get () {
+        return this.value
+      },
+
+      set (val) {
+        this.$emit('input', event.target.value)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.base-form-input {
+  &::placeholder {
+    font-size: 12px;
+    text-align: center;
+    letter-spacing: 1px;
+
+    @include media-breakpoint-up(md) {
+      font-size: 13px;
+      text-align: left;
+    }
+  }
+}
+</style>
