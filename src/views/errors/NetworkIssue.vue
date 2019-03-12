bac8a957e68e98ba03cc29ce260e6a5217b78f36
Iago <yagomello16@gmail.com>
2019-03-12 03:42:41 -0300

    error pages

---
+++ src/views/errors/NetworkIssue.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Uh-Oh!</h1>
+    <h3>
+      It looks like you're experiencing some network issues, please refresh your
+      page.
+    </h3>
+    <router-link :to="{ name: 'condominios' }">
+      or go back to Home Page
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NetworkIssue'
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/errors/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Oops!</h1>
+    <p>The {{ resource }} you're looking for is not here.</p>
+    <router-link :to="{ name: 'event-list' }">Back to Home Page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+
+  props: {
+    resource: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped></style>
